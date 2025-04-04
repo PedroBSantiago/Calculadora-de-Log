4e737ed32b3089cbf0df68aa8ddcf76c48e78662
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-04-04 20:12:26 -0300

    acabo

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+Console.Clear();
+double logaritmando, b, logaritmo;
+
+Console.WriteLine("----------calculadora de logaritmo----------\n\n");
+
+Console.Write("Base-----:");
+b = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("\n");
+
+Console.Write("logaritmando-----:");
+logaritmando = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("\n");
+logaritmo = Math.Log(logaritmando,b);
+
+Console.WriteLine($"resultado (logaritmo)= {logaritmo:N2}");
